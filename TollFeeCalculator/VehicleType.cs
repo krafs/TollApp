b7c9cbff701a445eab2cde4bd262f66d9575b00f
krafs <massa.krafs@proton.me>
2025-03-17 12:06:27 +0100

    Clean up unnecessary usings and adjust namespaces

--- TollFeeCalculator/VehicleType.cs
+++ TollFeeCalculator/VehicleType.cs
@@ -1,14 +1,13 @@
-namespace TollFeeCalculator
+namespace TollFeeCalculator;
+
+public enum VehicleType
 {
-    public enum VehicleType
-    {
-        None = 0,
-        Car = 1,
-        Motorbike = 2,
-        Tractor = 3,
-        Emergency = 4,
-        Diplomat = 5,
-        Foreign = 6,
-        Military = 7
-    }
+    None = 0,
+    Car = 1,
+    Motorbike = 2,
+    Tractor = 3,
+    Emergency = 4,
+    Diplomat = 5,
+    Foreign = 6,
+    Military = 7
 }
