41855d8ae829399b3691bdae5b3077b842d5602e
krafs <massa.krafs@proton.me>
2025-03-17 11:01:03 +0100

    Add missing VehicleType enum

---
+++ TollFeeCalculator/VehicleType.cs
@@ -0,0 +1,14 @@
+namespace TollFeeCalculator
+{
+    public enum VehicleType
+    {
+        None = 0,
+        Car = 1,
+        Motorbike = 2,
+        Tractor = 3,
+        Emergency = 4,
+        Diplomat = 5,
+        Foreign = 6,
+        Military = 7
+    }
+}
